f5da4501a263889f490e7add51530d576a5c7bb7
Ibrahim_bin_ajjaj <119033743+Frtook@users.noreply.github.com>
2025-01-13 09:41:35 +0300

    finish basic

--- app/tickets/TicketList.tsx
+++ app/tickets/TicketList.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
-      revalidate: 30,
+      revalidate: 0,
     },
   });
 
--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,16 +1,22 @@
 import { Suspense } from "react";
 import TicketList from "./TicketList";
 import Loading from "../loading";
+import Link from "next/link";
 
 export default function Tickets() {
   return (
     <main>
       <nav>
-        <div>
-          <h2>Tickets</h2>
-          <p>
-            <small>Currently open tickets.</small>
-          </p>
+        <div className="flex justify-between w-full items-center">
+          <div>
+            <h2>Tickets</h2>
+            <p>
+              <small>Currently open tickets.</small>
+            </p>
+          </div>
+          <Link className="btn-primary p-2 rounded-xl" href="tickets/create">
+            Create
+          </Link>
         </div>
       </nav>
       <Suspense fallback={<Loading />}>
@@ -19,4 +25,3 @@ export default function Tickets() {
     </main>
   );
 }
-//ghgjgkj
